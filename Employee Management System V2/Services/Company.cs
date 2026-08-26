bb25584e41c548d097508e409dde4276364ad6d3
Mona Mohamed <monamo20006@gmail.com>
2026-08-27 00:06:30 +0300

    Adding Company collections

--- Employee Management System V2/Services/Company.cs
+++ Employee Management System V2/Services/Company.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Employee_Management_System_V2.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,10 @@ namespace Employee_Management_System_V2.Services
 {
     internal class Company
     {
+        List<Employee> ActiveEmployees = new List<Employee>();
+        Dictionary<int, string> Departments = new Dictionary<int, string>();
+        Queue<Employee> OnBoarding = new Queue<Employee>();
+        Stack<string> ActionsHistory = new Stack<string>();
+        HashSet<string> UniqueSkills = new HashSet<string>();
     }
 }
